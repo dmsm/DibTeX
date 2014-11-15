7f3f49a51b9dc24267279c2055d4b3aac57af109
Noam Hurwitz <nhurwitz@gmail.com>
2014-11-15 02:25:47 -0800

    fixed writing to file

--- website/aggregator.py
+++ website/aggregator.py
@@ -1,4 +1,5 @@
 from models import Assignment, Problem, Submission
+from __future__ import print_function
 
 student_map = {}
 assignment_name = ""
@@ -15,38 +16,38 @@ def create_id_map(self, student_id, student_name):
     self.student_map[student_id] = student_name
 
 def print_header(submission_file):
-    print("\documentclass[12pt,letterpaper,boxed]{hmcpset}", submission_file)
-    print("% set 1-inch margins in the document", submission_file)
-    print("\usepackage[margin=1in]{geometry}", submission_file)
-    print("\usepackage{marvosym}", submission_file)
-    print("\usepackage{MnSymbol,wasysym}", submission_file)
-    print("\usepackage{tikz}", submission_file)
-    print("\usetikzlibrary{graphs,graphs.standard}", submission_file)
-    print("\usepackage{color}", submission_file)
-    print("\usepackage{enumerate}", submission_file)
-    print("\usepackage{tikz}", submission_file)
-    print("\usepackage{mathtools}", submission_file)
-    print("\DeclarePairedDelimiter\ceil{\lceil}{\\rceil}", submission_file)
-    print("\DeclarePairedDelimiter\\floor{\\lfloor}{\\rfloor}", submission_file)
-    print("% include this if you want to import graphics files with /includegraphics", submission_file)
+    print("\documentclass[12pt,letterpaper,boxed]{hmcpset}", file=submission_file)
+    print("% set 1-inch margins in the document", file=submission_file)
+    print("\usepackage[margin=1in]{geometry}", file=submission_file)
+    print("\usepackage{marvosym}", file=submission_file)
+    print("\usepackage{MnSymbol,wasysym}", file=submission_file)
+    print("\usepackage{tikz}", file=submission_file)
+    print("\usetikzlibrary{graphs,graphs.standard}", file=submission_file)
+    print("\usepackage{color}", file=submission_file)
+    print("\usepackage{enumerate}", file=submission_file)
+    print("\usepackage{tikz}", file=submission_file)
+    print("\usepackage{mathtools}", file=submission_file)
+    print("\DeclarePairedDelimiter\ceil{\lceil}{\\rceil}", file=submission_file)
+    print("\DeclarePairedDelimiter\\floor{\\lfloor}{\\rfloor}", file=submission_file)
+    print("% include this if you want to import graphics files with /includegraphics", file=submission_file)
 
 def aggregate(self, student_id, submissions):
     name = self.student_map[student_id]
     submission_file = open(name + self.assignment_name + self.TEX_FILE, 'a')
     print_header(submission_file)
 
-    print("\\name{" + name + "}", submission_file)
-    print("\duedate{" + assignment_due_date + "}", submission_file)
-    print("\\begin{document}", submission_file)
+    print("\\name{" + name + "}", file=submission_file)
+    print("\duedate{" + assignment_due_date + "}", file=submission_file)
+    print("\\begin{document}", file=submission_file)
 
     for submission in submissions:
         header = "\\begin{problem}[" + submission.problem.name + "]["+\
                  submission.score + "/" + submission.problem.points + "]\\"
-        print(header, submission_file)
-        print(submission.problem.contents, submission_file)
-        print("\end{problem}", submission_file)
-        print("\\begin{solution}", submission_file)
-        print(submission.problem.solution, submission_file)
-        print("\end{solution}", submission_file)
+        print(header, file=submission_file)
+        print(submission.problem.contents, file=submission_file)
+        print("\end{problem}", file=submission_file)
+        print("\\begin{solution}", file=submission_file)
+        print(submission.problem.solution, file=submission_file)
+        print("\end{solution}", file=submission_file)
 
-    print("\end{document}",submission_file)
+    print("\end{document}",file=submission_file)
