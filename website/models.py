f043f64f8abd6737f1666c1c60e0160169e3d509
Noam Hurwitz <nhurwitz@gmail.com>
2014-11-14 22:46:17 -0800

    Override Django's user class to allow for distinction between our users

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    USER_ROLE = (
+    	('P', 'Professer'),
+    	('G', 'Grader'),
+    	('S', 'Student')
+    )
+
+    # The additional attributes we wish to include.
+    type = models.CharField(max_length=1, choices=USER_ROLE)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
