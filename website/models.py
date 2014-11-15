a40b6d9f66e91db4bdf75154438f9fff966cb08c
Dima Smirnov <dimkasmir@gmail.com>
2014-11-15 00:13:14 -0800

    Create Assignment and Problem models

--- website/models.py
+++ website/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+CHARFIELD_MAX_LENGTH = 255
+
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
     USER_ROLE = (
-    	('P', 'Professer'),
+    	('P', 'Professor'),
     	('G', 'Grader'),
     	('S', 'Student')
     )
@@ -17,3 +19,21 @@ class UserProfile(models.Model):
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+
+class Problem(models.Model):
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    points = models.IntegerField()
+    solution = models.TextField()
+
+
+class Submission(models.Model):
+    student = models.ForeignKey(UserProfile)
+    problems = models.ManyToManyField(Problem)
+    contents = models.TextField()
+    score = models.IntegerField(null=True, blank=True)
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    due_date = models.DateTimeField()
+    problems = models.ManyToManyField(Problem)
