ea6d157fdef601cd398ec5158a6aad331f23ffad
Noam Hurwitz <nhurwitz@gmail.com>
2014-11-14 23:21:15 -0800

    Added backend functionality to login

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^test/', TemplateView.as_view(template_name="index.html")),
-    url(r'^register/$', views.register, name='register'), 
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'), 
 )
