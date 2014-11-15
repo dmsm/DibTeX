4a21c9be863cae565263faaebc851dbcec44294b
Noam Hurwitz <nhurwitz@gmail.com>
2014-11-15 02:28:59 -0800

    created package_problems.py

--- website/package_problems.py
+++ website/package_problems.py
@@ -1,1 +1,11 @@
-__author__ = 'noam'
+from __future__ import print_function
+from models import Assignment, Problem, Submission
+import aggregator
+
+def problem_answer_pairings(asgt):
+    a_n = asgt.name
+    for p in asgt.problems.all:
+        name = p.name
+        problem_file = open(a_n + "-" + name, 'a')
+        aggregator.print_header(problem_file)
+        print(p.content)
